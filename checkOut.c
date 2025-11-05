498fa3db7abce531f3859ec82f77889beb8141e2
SamuelCourtisPond <samuel.courtispond25@students.bhasvic.ac.uk>
2025-11-05 10:00:57 +0000

    Samuel 4/11/25

--- checkOut.c
+++ checkOut.c
@@ -1,5 +1,21 @@
 #include "checkOut.h"
 
+float totalCostRoom=0;
+float totalCostFood=0;
+int age=0;
+const int pensioner=65;
+const int minor=16;
+
+void foodCost() {
+
+}
+
+void foodRoom() {
+
+}
+
+
+
 #include <stdio.h>
 
 
