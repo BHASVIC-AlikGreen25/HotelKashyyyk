1399b926125b049c063edd7880fee755fb1ac475
Alik Green <alik.green25@students.bhasvic.ac.uk>
2025-11-05 09:59:14 +0000

    changes userInfo a bit

--- userInfo.c
+++ userInfo.c
@@ -1,1 +1,20 @@
 #include "userInfo.h"
+
+#include <string.h>
+
+char bookingIDs[6][256] = {0};
+
+bool isRoomAvailable(int roomNumber)
+{
+    return bookingIDs[roomNumber] == nullptr;
+}
+
+bool bookingIDExists(const char id[256])
+{
+    for(int i = 0; i < 6; i++)
+    {
+        if(strcmp(bookingIDs[i], id)) return true;
+    }
+
+    return false;
+}
--- userInfo.h
+++ userInfo.h
@@ -1,4 +1,10 @@
 #ifndef USERINFO_H
 #define USERINFO_H
 
+bool isRoomAvailable(int roomNumber);
+bool bookingIDExists(const char id[200]);
+
+void makeRoomAvailiable();
+void assignRoomToBookingID(const char id[200]);
+
 #endif
